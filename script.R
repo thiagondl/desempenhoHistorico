767d70a9407d3205d5b7a0c9ac40095774d219f8
thiagondl <thiago.lima@ccc.ufcg.edu.br>
2021-09-01 23:42:58 -0300

    finaliza pre processamento do pdf

--- script.R
+++ script.R
@@ -51,19 +51,66 @@ for(i in 1:length(dataPdf)) {
 }
 
 cleanLines <- unlist(cleanLines)
-column_index <- list()
+column1 <- c()
+column2 <- c()
+column3 <- c()
+column4 <- c()
+column5 <- c()
+column6 <- c()
+column7 <- c()
+column8 <- c()
 
 for(e in cleanLines) {
     matches <- regexpr("[0-9][0-9][0-9][0-9][0-9][0-9][0-9]", e)
     length <- attr(matches,"match.length")
-    column1 <- substr(e, matches[1], length + 1)
+    column1 <- c(column1, substr(e, matches[1], length + 1))
     e <- substring(e, matches[1] + length + 1)
     
     matches <- gregexpr("^.*(?=( Optativa | Obrigatória))", e, perl = TRUE)[[1]]
     length <- attr(matches,"match.length")
-    column2 <- substr(e, matches[1], length)
+    column2 <- c(column2, substr(e, matches[1], length))
+    e <- substring(e, matches[1] + length + 1)
+    
+    matches <- gregexpr("Optativa|Obrigatória", e)[[1]]
+    length <- attr(matches,"match.length")
+    column3 <- c(column3, substr(e, matches[1], length))
+    e <- substring(e, matches[1] + length + 1)
+    
+    matches <- gregexpr("^.*(?=( [0-9][0-9] ))", e, perl = TRUE)[[1]]
+    length <- attr(matches,"match.length")
+    column4 <- c(column4, substr(e, matches[1], length))
+    e <- substring(e, matches[1] + length + 1)
+    
+    matches <- gregexpr("^.*(?=( - | [0-9]*,[0-9]))", e, perl = TRUE)[[1]]
+    length <- attr(matches,"match.length")
+    column5 <- c(column5, substr(e, matches[1], length))
+    e <- substring(e, matches[1] + length + 1)
+    
+    matches <- gregexpr("-|[0-9]*,[0-9]", e, perl = TRUE)[[1]]
+    length <- attr(matches,"match.length")
+    column6 <- c(column6, substr(e, matches[1], length))
+    e <- substring(e, matches[1] + length + 1)
+    
+    matches <- gregexpr("^.*(?=( [0-9]*.[0-9]))", e, perl = TRUE)[[1]]
+    length <- attr(matches,"match.length")
+    column7 <- c(column7, substr(e, matches[1], length))
     e <- substring(e, matches[1] + length + 1)
 
+    column8 <- c(column8, e)
 }
 
+data <-
+    as_tibble(
+        list(
+            "codigo_disc" = column1,
+            "nome_disc" = column2,
+            "tipo_cod" = column3,
+            "credito" = column4,
+            "ch" = column5,
+            "media" = column6,
+            "situacao" = column7,
+            "periodo" = column8
+        )
+    )
 
+write_csv(data, "data/data.csv")
